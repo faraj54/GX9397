d1bd95b07d640a342b0fbbefd24a6030dccd6e3e
Kassem Faraj <faraj54@gmail.com>
2020-03-19 01:21:56 -0400

    seconde commit

---
+++ introduction/src/introduction/HelloJava.java
@@ -0,0 +1,12 @@
+package introduction;
+
+public class HelloJava {
+	 
+	  public static void main(String[] args) {
+	 
+		  System.out.print("Welcome ");
+	  System.out.print("Type your name here");
+	 }
+	 
+
+}
